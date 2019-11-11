119db38bcd8a486d9d90813516c56f1bd11437e1
silk92 <406094505@qq.com>
2019-11-11 10:01:45 +0800

    完成批量申请功能

--- src/main/java/com/ximingxing/blog/server/dao/RecordMapper.java
+++ src/main/java/com/ximingxing/blog/server/dao/RecordMapper.java
@@ -1,7 +1,9 @@
 package com.ximingxing.blog.server.dao;
 
 import com.ximingxing.blog.server.pojo.Record;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface RecordMapper {
     int deleteByPrimaryKey(Integer conferenceId);
 
